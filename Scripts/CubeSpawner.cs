58c62f83e511de3c1262ec1e70e0e66ac4734ed0
Necollaz <156367647+Necollaz@users.noreply.github.com>
2024-05-20 16:26:21 +0300

    Update CubeSpawner.cs

--- Scripts/CubeSpawner.cs
+++ Scripts/CubeSpawner.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private Cube _cubePrefab;
-    [SerializeField] private int minCubes = 2;
-    [SerializeField] private int maxCubes = 6;
+    [SerializeField] private int _minCubes = 2;
+    [SerializeField] private int _maxCubes = 6;
     [SerializeField] private float _scaledownFactor = 0.5f;
     [SerializeField] private float _invisibleWallDistance = 5f;
 
@@ -20,7 +20,7 @@ public class CubeSpawner : MonoBehaviour
 
     public void SpawnCubes(Vector3 explosionPosition, float parentSplitChance, Vector3 parentScale)
     {
-        int numOfCubes = Random.Range(minCubes, maxCubes + 1);
+        int numOfCubes = Random.Range(_minCubes, _maxCubes + 1);
         float newSplitChance = parentSplitChance / 2f;
         Vector3 newScale = parentScale * _scaledownFactor;
 
